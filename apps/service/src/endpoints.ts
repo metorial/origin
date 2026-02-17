a5661d5d0a45cc4785dd3bc4055bab91e8beaf26
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-02-17 17:16:07 +0100

    Fix redis pings

--- apps/service/src/endpoints.ts
+++ apps/service/src/endpoints.ts
@@ -1,4 +1,4 @@
-import { redis } from 'bun';
+import { RedisClient } from 'bun';
 import { originApi } from './controllers';
 import { db } from './db';
 import { scmController } from './public/scm';
@@ -23,7 +23,12 @@ Bun.serve({
   fetch: async _ => {
     try {
       await db.tenant.count();
+
+      let redis = new RedisClient(process.env.REDIS_URL?.replace('rediss://', 'redis://'), {
+        tls: process.env.REDIS_URL?.startsWith('rediss://')
+      });
       await redis.ping();
+
       return new Response('OK');
     } catch (e) {
       return new Response('Service Unavailable', { status: 503 });
